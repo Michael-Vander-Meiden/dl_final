bd5f58f76399185b3349724e6a16926dc18d0b69
michael <mavm@cmu.edu>
2017-12-01 01:36:07 -0500

    here we gooooo

--- dreamar.py
+++ dreamar.py
@@ -78,7 +78,7 @@ if __name__ == "__main__":
   parser.add_argument('-s', '--source',      help='video source')
   parser.add_argument('-d', '--destination', help='video destination')
   parser.add_argument('-c', '--config',      help='style config',     default='{}')
-  parser.add_argument('-u', '--uuid',        help='instance uuid',    default=)
+  parser.add_argument('-u', '--uuid',        help='instance uuid',    default=0)
 
   args = parser.parse_args()
 
